e38e6fa3dcc0543d9c61c1c076c0973b65d730ef
Landon <33662627+landonviator@users.noreply.github.com>
2022-03-09 18:54:04 -0600

    updated button text alpha

--- viator_modules/viator_gui/Widgets/PushButton.h
+++ viator_modules/viator_gui/Widgets/PushButton.h
@@ -24,8 +24,8 @@ namespace viator_gui
             setColour(0x1000100, juce::Colours::whitesmoke.withAlpha(0.25f));
             setColour(0x1000c00, juce::Colours::whitesmoke.withAlpha(0.06f));
             setColour(0x1000101, juce::Colours::lightgoldenrodyellow.darker(0.2f));
-            setColour(0x1000102, juce::Colours::black.brighter(0.1));
-            setColour(0x1000103, juce::Colours::black.brighter(0.1));
+            setColour(0x1000102, juce::Colours::black.brighter(0.1).withAlpha(0.85f));
+            setColour(0x1000103, juce::Colours::black.brighter(0.1).withAlpha(0.85f));
             setLookAndFeel(&customButton);
         }
         
