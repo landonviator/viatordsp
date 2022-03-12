43fd9ab277fc7ccaf5ee6552e309a0d1533c0ba9
Landon <33662627+landonviator@users.noreply.github.com>
2022-03-11 18:32:05 -0600

    added FastMath namespace in Utils

--- viator_modules/viator_utils/utils.h
+++ viator_modules/viator_utils/utils.h
@@ -63,6 +63,25 @@ namespace viator_utils
             }
         }
     };
+
+    struct FastMath
+    {
+        /** Fast std::pow*/
+        static inline double fastPow(double a, double b)
+        {
+            union
+            {
+                double d;
+                int x[2];
+            }
+            
+            u = { a };
+            u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
+            u.x[0] = 0;
+            
+            return u.d;
+        }
+    };
 }
 
 #endif /* utils_h */
