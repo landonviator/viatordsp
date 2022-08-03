4a9b72923923364932e652a86f1353cb43759720
Landon <33662627+landonviator@users.noreply.github.com>
2022-08-02 21:22:28 -0500

    update

--- viator_modules/viator_dsp/BitCrusher.h
+++ viator_modules/viator_dsp/BitCrusher.h
@@ -26,24 +26,79 @@ public:
         {
             for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
             {
+                
+                auto rateDivide = static_cast<int> (_resample.getNextValue());
+                
+                // Dry signal
+                float _drySignal = data[ch][sample];
+                
+                // Reduce bit depth
+                float totalQLevels = std::powf(2, _bitDepth.getNextValue());
+                
+                // Wet signal
+                float _wetSignal = data[ch][sample];
+                
+                _wetSignal = 0.5 * _wetSignal + 0.5;
+                
+                _wetSignal = totalQLevels * _wetSignal;
+                
+                _wetSignal = (std::round(_wetSignal) / totalQLevels) * 2 - 1;
+                
+                float remainder = std::fmodf(_wetSignal, 1.0 / totalQLevels);
+                
+                _wetSignal -= remainder;
+                
+                if (rateDivide > 1)
                 {
-                    data[ch][sample] = processSample(data[ch][sample], ch);
+                    if (sample % rateDivide != 0)
+                    {
+                        _wetSignal = data[ch][sample - sample % rateDivide];
+                    }
                 }
+                
+                data[ch][sample] = _wetSignal;
             }
         }
     }
     
     /** Process an individual sample */
-    SampleType processSample(SampleType input, int ch) noexcept
+    SampleType processSample(SampleType input, int ch, int sample) noexcept
     {
-        return input * 2;
+        setResampleRate(_resample.getNextValue() + (_currentSampleRate - _resample.getNextValue()));
+        
+        // Dry signal
+        float _drySignal = input;
+        
+        // Wet signal
+        float _wetSignal = input;
+        
+        // Reduce bit depth
+        float totalQLevels = std::powf(2, _bitDepth.getNextValue());
+        
+        _wetSignal = 0.5 * _wetSignal + 0.5;
+        
+        _wetSignal = totalQLevels * _wetSignal;
+        
+        _wetSignal = (std::round(_wetSignal) / totalQLevels) * 2 - 1;
+        
+        return _wetSignal;
     }
+    
+    void setBitDepth(SampleType newBitDepth);
+    void setResampledRate(SampleType newResampleRate);
     
 private:
     juce::SmoothedValue<float> _drive;
+    juce::SmoothedValue<float> _bitDepth;
     juce::SmoothedValue<float> _mix;
     juce::SmoothedValue<float> _output;
+    juce::SmoothedValue<float> _resample;
     float _currentSampleRate;
+    int _previousSample = 0;
+    float _rateDivide;
+    void setResampleRate(SampleType newResampleRate);
+    
+    juce::NormalisableRange<float> _bitRateRange;
 };
 }
 #endif /* BitCrusher_hpp */
