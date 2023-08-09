7e3831933714bc8731b03aee0709d0d338fcbddb
Landon <33662627+landonviator@users.noreply.github.com>
2023-08-08 23:06:17 -0500

    add way to set text size on width or height of slider

--- viator_modules/viator_gui/LAF/DialLAF.cpp
+++ viator_modules/viator_gui/LAF/DialLAF.cpp
@@ -215,7 +215,6 @@ void CustomDial::drawLabel (juce::Graphics& g, juce::Label& label)
 #pragma mark Dial Just Label
 CustomDialLabel::CustomDialLabel()
 {
-    
 }
 
 void CustomDialLabel::drawLabel (juce::Graphics& g, juce::Label& label)
@@ -227,7 +226,7 @@ void CustomDialLabel::drawLabel (juce::Graphics& g, juce::Label& label)
     if (! label.isBeingEdited())
     {
         auto alpha = label.isEnabled() ? 1.0f : 0.5f;
-        const juce::Font font (juce::Font ("Helvetica", label.getWidth() * 0.12, juce::Font::FontStyleFlags::bold));
+        const juce::Font font (juce::Font ("Helvetica", juce::jmax(label.getWidth() * 0.12, label.getHeight() * 0.25), juce::Font::FontStyleFlags::bold));
 
         g.setColour (label.findColour (Label::textColourId).withMultipliedAlpha (alpha));
         g.setFont (font);
